38d81ce24da26c7c3a60fae4dd87847d7dd4c7e5
jared <jcraig1730@gmail.com>
2019-12-11 14:14:25 -0800

    refactor WeatherMap to be functional component that can use state provided by hooks

--- ui/components/current/weatherMap/WeatherMap.jsx
+++ ui/components/current/weatherMap/WeatherMap.jsx
@@ -1,40 +1,63 @@
-import React, { Component } from "react";
+// this component needs to be updated to a function in order to
+// use hooks to set the state for zip
+
+import React, { useEffect, useState } from "react";
 import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
 import { googleAPIKey, weatherAPI } from "../../../../keys.js";
 import Axios from "axios";
+import { useStateValue } from "../../../state/AppState.jsx";
 
-class Scratch extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isLoading: true,
-      coords: { lat: "", lng: "" },
-      zoom: 12,
-      features: []
-    };
-    this.handleMapClick = this.handleMapClick.bind(this);
-  }
+function Scratch(props) {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     isLoading: true,
+  //     coords: { lat: "", lng: "" },
+  //     zoom: 12,
+  //     features: []
+  //   };
+  const [isLoading, setIsLoading] = useState(true);
+  const [coords, setCoords] = useState({ lat: "", lng: "" });
+  const [zoom, setZoom] = useState(12);
+  const [features, setFeatures] = useState([]);
+  const [{ zip }, dispatch] = useStateValue();
+  // this.handleMapClick = this.handleMapClick.bind(this);
+  // this.handleMarkerClick = this.handleMarkerClick.bind(this);
 
-  handleMapClick(e) {
+  const handleMapClick = e => {
     console.log(e);
-  }
+  };
+  const handleMarkerClick = e => {
+    console.log(e);
+    const { lat, lng } = e.position;
+    console.log(lat, lng);
+    Axios.get(
+      // "http://localhost:3001/loctozip"
+      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${googleAPIKey}`
+    ).then(({ data }) => {
+      console.log(data);
+      const zip = data.results[0].address_components[6].long_name;
+      console.log(zip);
+      dispatch(zip, "SET_ZIP");
+    });
+  };
 
-  getWeather(northLat, eastLng, southLat, westLng) {
+  const getWeather = (northLat, eastLng, southLat, westLng) => {
     return new Promise((resolve, reject) => {
       Axios.get(
-        `http://api.openweathermap.org/data/2.5/box/city?bbox=${westLng},${northLat},${eastLng},${southLat},${this.state.zoom}&cluster=yes&format=json&APPID=${weatherAPI}`
+        `http://api.openweathermap.org/data/2.5/box/city?bbox=${westLng},${northLat},${eastLng},${southLat},${zoom}&cluster=yes&format=json&APPID=${weatherAPI}`
       ).then(data => {
+        console.log(data);
         const newFeatures = data.data.list.map(dataPoint =>
-          this.jsonToGeoJson(dataPoint)
+          jsonToGeoJson(dataPoint)
         );
-        this.setState({ features: newFeatures });
-
+        setFeatures(newFeatures);
         resolve();
       });
     }).catch(err => console.log(err));
-  }
+  };
 
-  jsonToGeoJson(weatherItem) {
+  const jsonToGeoJson = weatherItem => {
     var feature = {
       type: "Feature",
       properties: {
@@ -61,76 +84,66 @@ class Scratch extends Component {
     };
     // returns object
     return feature;
-  }
+  };
 
-  centerMoved(mapProps, map) {
+  const centerMoved = (mapProps, map) => {
     console.log(mapProps.geometry);
+    console.log(mapProps);
     console.log(map.center.lat(), map.center.lng());
-  }
+  };
 
-  componentDidMount() {
-    // Axios.get(
-    //   `https://tile.openweathermap.org/map/clouds/5/1/1.png?appid=${weatherAPI}`
-    // ).then(data => {
-    //   Axios.post("http://localhost:3001/getdata", data);
-    // });
-    console.log(this.props.auth, "hi");
+  useEffect(() => {
     Axios.get(`http://localhost:3001/geo`)
       .then(data => {
-        // const { northeast, southwest } = data.data.results[0].geometry.bounds;
-        const northeast = { lat: 37.8, lng: -122.39 };
-        const southwest = { lat: 37.702, lng: -122.49395 };
-        this.getWeather(
-          northeast.lat,
-          northeast.lng,
-          southwest.lat,
-          southwest.lng
+        const center = data.data.results[0].geometry.location;
+        // the largest area the free weather api will allow is 5 deg squared, so add and subtract 2.5 to the center
+        // to find the northeast and southeast corners
+        getWeather(
+          center.lat + 2.5,
+          center.lng + 2.5,
+          center.lat - 2.5,
+          center.lng - 2.5
         ).then(() => {
-          this.setState({
-            coords: data.data.results[0].geometry.location,
-            isLoading: false
-          });
+          setCoords(center);
+          setIsLoading(false);
         });
       })
       .catch(err => console.log(err));
-  }
-  render() {
-    const test = { lat: 37.771, lng: -122.4318 };
-    let { lat, lng } = this.state.coords;
-    lat = Number(lat);
-    lng = Number(lng);
-    const mapStyles = { width: "60vw", height: "476px" };
-    const MapImage = (
-      <div className="contaner">
-        <Map
-          id="mymap"
-          google={this.props.google}
-          zoom={this.state.zoom}
-          style={mapStyles}
-          initialCenter={test}
-          features={this.state.features}
-          // initialCenter={{ lat: lat.toFixed(1), lng: lng.toFixed(1) }}
-          onClick={this.handleMapClick}
-          onDragend={this.centerMoved}
-        >
-          <Marker position={{ lat: 37.762391, lng: -122.439192 }} />
-          {this.state.features.map(feature => {
-            const [lng, lat] = feature.geometry.coordinates;
-            return (
-              <Marker
-                title={feature.properties.city}
-                name={feature.properties.city}
-                position={{ lng, lat }}
-                icon={{ url: feature.properties.icon }}
-              />
-            );
-          })}
-        </Map>
-      </div>
-    );
+  }, []);
 
-    return this.state.isLoading ? <div>loading</div> : <div>{MapImage}</div>;
-  }
+  let { lat, lng } = coords;
+  lat = Number(lat);
+  lng = Number(lng);
+  const mapStyles = { width: "60vw", height: "476px" };
+  const MapImage = (
+    <div className="">
+      <Map
+        id="mymap"
+        google={props.google}
+        zoom={zoom}
+        style={mapStyles}
+        initialCenter={coords}
+        features={features}
+        onClick={handleMapClick}
+        onDragend={centerMoved}
+      >
+        {features.map(feature => {
+          const [lng, lat] = feature.geometry.coordinates;
+          return (
+            <Marker
+              title={feature.properties.city}
+              name={feature.properties.city}
+              position={{ lng, lat }}
+              icon={{ url: feature.properties.icon }}
+              onClick={handleMarkerClick}
+            />
+          );
+        })}
+      </Map>
+    </div>
+  );
+
+  return isLoading ? <div>loading</div> : <div>{MapImage}</div>;
 }
 
 export default GoogleApiWrapper({ apiKey: googleAPIKey })(Scratch);
